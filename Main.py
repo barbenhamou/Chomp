66f9a7a678ead757c0e470b97fec51c02502bf7f
barbenhamou <barbenhamou2006@gmail.com>
2024-07-15 00:39:46 +0300

    finally

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 import pygame
-from Rectangle import Rectangle
 import time
 
 WIDTH = 750
@@ -14,6 +13,19 @@ background_color = (0, 0, 0)
 grid = []
 screen = None
 running = True
+
+class Rectangle:
+    def __init__(self, UpLeft: tuple, DownRight: tuple, color: tuple, screen):
+        self.UpLeft = UpLeft
+        self.DownRight = DownRight
+        self.color = color
+        self.screen = screen
+
+    def show(self):
+        pygame.draw.rect(self.screen, self.color, pygame.Rect(self.UpLeft[0] + 1, self.UpLeft[1] + 1, self.DownRight[0] - self.UpLeft[0], self.DownRight[1] - self.UpLeft[1]))
+
+    def get_eaten(self):
+        self.color = ORANGE
 
 def init_screen():
     global screen
